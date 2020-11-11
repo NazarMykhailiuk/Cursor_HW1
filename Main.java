10282fb6bd44cb5c24bf52a951befc2307f8b1c1
NazarMyhailiuk <nazar.mykhailiuk@gmail.com>
2020-11-12 00:07:45 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int intValue = 129;
+        byte value = (byte)intValue;
+        System.out.println(value);
+    }
+}
