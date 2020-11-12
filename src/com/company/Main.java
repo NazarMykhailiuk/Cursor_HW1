7a9b98d9a4729b5b0586e8cdaee756589ad90ed2
NazarMyhailiuk <nazar.mykhailiuk@gmail.com>
2020-11-12 19:35:44 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,48 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("HW");
+        System.out.println("BYTE TYPE");
+        Byte b1 = 6;
+        Byte b2 = 4;
+        System.out.println("Addition: " + b1 + " + " + b2 + " = " + (b1 + b2));
+        System.out.println("Subtraction: " + b1 + " - " + b2 + " = " + (b1 - b2));
+        System.out.println("Multiplication: " + b1 + " x " + b2 + " = " + (b1 * b2));
+        System.out.println("Division: " + b1 + " / " + b2 + " = " + (b1 / b2));
+        System.out.println("");
+
+        System.out.println("SHORT TYPE");
+        short s1 = 255;
+        short s2 = 21;
+        System.out.println("Addition: " + s1 + " + " + s2 + " = " + (s1 + s2));
+        System.out.println("Subtraction: " + s1 + " - " + s2 + " = " + (s1 - s2));
+        System.out.println("Multiplication: " + s1 + " x " + s2 + " = " + (s1 * s2));
+        System.out.println("Division: " + s1 + " / " + s2 + " = " + (s1 / s2));
+        System.out.println("");
+
+        System.out.println("LONG TYPE");
+        long l1 = 256458L;
+        long l2 = 5568L;
+        System.out.println("Addition: " + l1 + " + " + l2 + " = " + (l1 + l2));
+        System.out.println("Subtraction: " + l1 + " - " + l2 + " = " + (l1 - l2));
+        System.out.println("Multiplication: " + l1 + " x " + l2 + " = " + (l1 * l2));
+        System.out.println("Division: " + l1 + " / " + l2 + " = " + (l1 / l2));
+        System.out.println("");
+
+        System.out.println("DOUBLE TYPE");
+        double d1 = 5889785.458d;
+        double d2 = 98754.255d;
+        System.out.println("Addition: " + d1 + " + " + d2 + " = " + (d1 + d2));
+        System.out.println("Subtraction: " + d1 + " - " + d2 + " = " + (d1 - d2));
+        System.out.println("Multiplication: " + d1 + " x " + d2 + " = " + (d1 * d2));
+        System.out.println("Division: " + d1 + " / " + d2 + " = " + (d1 / d2));
+        System.out.println("");
+
+        System.out.println("FLOAT TYPE");
+        float f1 = 542.254f;
+        float f2 = 264.6558f;
+        System.out.println("Addition: " + f1 + " + " + f2 + " = " + (f1 + f2));
+        System.out.println("Subtraction: " + f1 + " - " + f2 + " = " + (f1 - f2));
+        System.out.println("Multiplication: " + f1 + " x " + f2 + " = " + (f1 * f2));
+        System.out.println("Division: " + f1 + " / " + f2 + " = " + (f1 / f2));
     }
 }
